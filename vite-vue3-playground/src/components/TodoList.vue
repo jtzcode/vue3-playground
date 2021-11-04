3100f80e229ed1f7091c49296785ee108557d5a8
citrix <citrix@soulerys-mini.citrite.net>
2021-11-04 11:27:19 +0800

    learn composition api

---
+++ vite-vue3-playground/src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>  
+    <div>
+        <h1 @click="add">{{count}}</h1>
+        <h2>Things to do: {{taskCount}}</h2>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed} from 'vue';
+import { useTodos } from '../utils/todos';
+let count = ref(1);
+let color = ref("red");
+function add() {
+    count.value++;
+    color.value = Math.random() > 0.5 ? "red" : "green";
+}
+
+let todoObj = useTodos();
+let taskCount = todoObj.all;
+
+
+</script>
+
+<style scoped>
+    h1 {  
+        color: v-bind(color);
+    }
+</style>
